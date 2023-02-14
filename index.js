b51db0ae1487ea6843ba9d0df5720839c71f7163
Arun <saharan.arun1124@gmail.com>
2023-02-14 17:34:13 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let input = '';
+
+let buttons = document.querySelectorAll('.button');
+
+Array.from(buttons).forEach((btn)=>{
+  btn.addEventListener('click',(e)=>{
+    if(e.target.innerHTML == '='){
+      input = eval(input);
+      document.querySelector('.input-display').value = input;
+    }else if(e.target.innerHTML == 'C'){
+      input = '';
+      document.querySelector('.input-display').value = input;
+    }else{
+      input = input + e.target.innerHTML;
+      document.querySelector('.input-display').value = input;
+    }
+
+  })
+})
