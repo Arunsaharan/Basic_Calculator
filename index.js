025f0f06ef567af9042f59c83861b79dd2b56f8c
Arun <saharan.arun1124@gmail.com>
2023-02-15 17:19:52 +0530

    First update - Made responsive to small screens

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ let input = '';
 
 let buttons = document.querySelectorAll('.button');
 
-Array.from(buttons).forEach((btn)=>{
-  btn.addEventListener('click',(e)=>{
-    if(e.target.innerHTML == '='){
+Array.from(buttons).forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    if (e.target.innerHTML == '=') {
       input = eval(input);
       document.querySelector('.input-display').value = input;
-    }else if(e.target.innerHTML == 'C'){
+    } else if (e.target.innerHTML == 'C') {
       input = '';
       document.querySelector('.input-display').value = input;
-    }else{
+    } else {
       input = input + e.target.innerHTML;
       document.querySelector('.input-display').value = input;
     }
